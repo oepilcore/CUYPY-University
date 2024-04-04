c39da6d9669276e093bf34f8f7919eb0530d0aa9
oepilcore <kasteraaa13@gmail.com>
2024-04-05 00:58:37 +0700

    menambahkan file tugas

--- Sesi1/main.py
+++ Sesi1/main.py
@@ -15,10 +15,18 @@ Hi {nama_user}! Coba perhatikan goa di bawah ini
 ''')
 
 pilihan_user = int(input("Menurut anda dimana CUYPY berada? [ 1 / 2 / 3 / 4 ]: "))
+konfirmasi_user = input(f"apakah anda sudah yakin pilihan anda {pilihan_user}? [ y / n ]: ")
 
-print(f"pilihan anda adalah {pilihan_user} ")
-
-if pilihan_user == cuypy_position:
-    print(f"SELAMAT {nama_user} ANDA BENAR! Posisi CUYPY berada di {cuypy_position} dan pilihan anda adalah goa nomor {pilihan_user} " )
+if konfirmasi_user == "y":
+    print(f"pilihan anda adalah {pilihan_user}")
+    if pilihan_user == cuypy_position:
+        print(f"SELAMAT {nama_user} ANDA BENAR! Posisi CUYPY berada di {cuypy_position} dan pilihan anda adalah goa nomor {pilihan_user} " )
+    else:
+        print(f"MAAF {nama_user} ANDA SALAH! CUYPY bukan berada di sana, melainkan berad di {cuypy_position}")
 else:
-    print(f"MAAF {nama_user} ANDA SALAH! CUYPY bukan berada di sana, melainkan berad di {cuypy_position}")
+    pilihan_user = int(input("Menurut anda dimana CUYPY berada? [ 1 / 2 / 3 / 4 ]: "))
+    print(f"pilihan anda adalah {pilihan_user}")
+    if pilihan_user == cuypy_position:
+        print(f"SELAMAT {nama_user} ANDA BENAR! Posisi CUYPY berada di {cuypy_position} dan pilihan anda adalah goa nomor {pilihan_user} " )
+    else:
+        print(f"MAAF {nama_user} ANDA SALAH! CUYPY bukan berada di sana, melainkan berad di {cuypy_position}")
