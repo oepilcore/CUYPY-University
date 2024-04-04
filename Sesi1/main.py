cd2cc2900ae18fd609819ca12310240f145df2b3
Muhammad Aji Prasetyo <75011718+oepilcore@users.noreply.github.com>
2024-04-05 00:16:09 +0700

    Add Sesi 1 and main.py via upload file

---
+++ Sesi1/main.py
@@ -0,0 +1,24 @@
+import random
+
+welcome_message = "Welcome to Cuypy Games!"
+cuypy_position = random.randint(1, 4)
+
+print("=============================")
+print(f"== {welcome_message} ==")
+print("=============================")
+
+nama_user = input("masukkan nama anda: ")
+
+print(f'''
+Hi {nama_user}! Coba perhatikan goa di bawah ini
+|_| |_| |_| |_|
+''')
+
+pilihan_user = int(input("Menurut anda dimana CUYPY berada? [ 1 / 2 / 3 / 4 ]: "))
+
+print(f"pilihan anda adalah {pilihan_user} ")
+
+if pilihan_user == cuypy_position:
+    print(f"SELAMAT {nama_user} ANDA BENAR! Posisi CUYPY berada di {cuypy_position} dan pilihan anda adalah goa nomor {pilihan_user} " )
+else:
+    print(f"MAAF {nama_user} ANDA SALAH! CUYPY bukan berada di sana, melainkan berad di {cuypy_position}")
